6cbac0f475d80f54bc0d1cdc10190431d57b307d
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2019-12-27 21:50:09 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+let main = document.querySelector('.container');
+
+let searchIcon = document.querySelector('.search-icon');
+let searchBox = document.querySelector('.search-bar');
+
+searchIcon.addEventListener('click', function () {
+    searchBox.classList.toggle('hide');
+});
+// rendering employees
+let employees = [];
+let counter = 0;
+
+async function getData () { 
+        $.ajax({
+            /* The whisperingforest.org URL is not longer valid, I found a new one that is similar... */
+            url: 'https://randomuser.me/api/',
+            async: true,
+            dataType: 'json',
+            success: function (data) {
+                employees.push(data.results[0]);
+                counter++;
+                if (counter < 12) {
+                    getData();
+                } else {
+                    displayData();
+                }               
+            }
+        });
+}
+
+ function displayData() {
+     for (let x = 0; x < employees.length; x++) {
+         console.log(employees[x]);
+
+         main.innerHTML += `  <div class="card">
+                    <img src="${employees[x].picture.large}" />
+                    <div class="card-body">
+                        <h3>${employees[x].name.first} ${employees[x].name.last}</h3>
+                        <p>${employees[x].email}</p>
+                    </div>
+                </div>`;
+    }
+}
+
+
+
+ getData();
